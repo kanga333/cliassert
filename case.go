0f3e10ad0b19044baa779c0afbaece85829cf7a3
kanga333 <e411z7t40w@gmail.com>
2018-07-23 23:46:53 +0900

    Add assert case

--- case.go
+++ case.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"regexp"
+	"strings"
+)
+
 type testCase interface {
 	assert(input string) bool
+}
+
+type containCase struct {
+	substr string
+	isNot  bool
+}
+
+func (c *containCase) assert(input string) bool {
+	ok := strings.Contains(input, c.substr)
+	if c.isNot {
+		return !ok
+	}
+	return ok
+}
+
+type regexCase struct {
+	patern regexp.Regexp
+	isNot  bool
+}
+
+func (c *regexCase) assert(input string) bool {
+	ok := c.patern.MatchString(input)
+	if c.isNot {
+		return !ok
+	}
+	return ok
 }
