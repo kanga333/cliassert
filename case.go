8da9d1dcbad47c0014515c594aafdfa198df7e77
kanga333 <e411z7t40w@gmail.com>
2018-07-24 23:04:32 +0900

    Add newCase

--- case.go
+++ case.go
@@ -14,6 +14,13 @@ type containCase struct {
 	isNot  bool
 }
 
+func newContainCase(substr string, isNot bool) *containCase {
+	return &containCase{
+		substr: substr,
+		isNot:  isNot,
+	}
+}
+
 func (c *containCase) assert(input string) bool {
 	ok := strings.Contains(input, c.substr)
 	if c.isNot {
@@ -23,12 +30,24 @@ func (c *containCase) assert(input string) bool {
 }
 
 type regexCase struct {
-	patern regexp.Regexp
-	isNot  bool
+	pattern *regexp.Regexp
+	isNot   bool
+}
+
+func newRegexCase(pattern string, isNot bool) (*regexCase, error) {
+	r, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	return &regexCase{
+		pattern: r,
+		isNot:   isNot,
+	}, nil
 }
 
 func (c *regexCase) assert(input string) bool {
-	ok := c.patern.MatchString(input)
+	ok := c.pattern.MatchString(input)
 	if c.isNot {
 		return !ok
 	}
