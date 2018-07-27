129effdf1e52b22022aedbec81afb8aa981e6b69
kanga333 <e411z7t40w@gmail.com>
2018-07-27 15:05:56 +0900

    Add assetion

---
+++ asserttion.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+type assertion struct {
+	expectCode  int
+	stdoutCases []assertCase
+	stderrCases []assertCase
+}
+
+func newAssertion(code int) *assertion {
+	a := &assertion{
+		expectCode: code,
+	}
+	return a
+}
+
+func (a *assertion) appendStdoutCase(ac assertCase) {
+	a.stdoutCases = append(a.stdoutCases, ac)
+}
+
+func (a *assertion) appendStderrCase(ac assertCase) {
+	a.stderrCases = append(a.stderrCases, ac)
+}
+
+func (a *assertion) assertCliResult(r result) (bool, string) {
+	if !r.assertReturnCode(a.expectCode) {
+		return false, fmt.Sprintln("return code should be ", a.expectCode)
+	}
+
+	for _, c := range a.stdoutCases {
+		if !r.assertStdout(c) {
+			return false, c.describe()
+		}
+	}
+
+	for _, c := range a.stderrCases {
+		if !r.assertStderr(c) {
+			return false, c.describe()
+		}
+	}
+	return true, ""
+}
