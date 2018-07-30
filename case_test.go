36d7fc993e51c5b18dce0367aa2e832ad68c9d96
kanga333 <e411z7t40w@gmail.com>
2018-07-30 21:59:30 +0900

    Add case tests

--- case_test.go
+++ case_test.go
@@ -5,17 +5,81 @@ import (
 )
 
 func TestContainCase(t *testing.T) {
-	c := newContainCase("t")
-	okCase := "test"
-	ngCase := "ng"
-
-	got := c.assert(okCase)
-	if got != true {
-		t.Errorf("Assert %s got: %v, want: true:", okCase, got)
+	cases := []struct {
+		subString string
+		input     string
+		want      bool
+	}{
+		{subString: "t", input: "test", want: true},
+		{subString: "n", input: "test", want: false},
 	}
 
-	got = c.assert(ngCase)
-	if got != false {
-		t.Errorf("Assert %s got: %v, want: false:", ngCase, got)
+	for _, c := range cases {
+		cc := newContainCase(c.subString)
+		got := cc.assert(c.input)
+		if got != c.want {
+			t.Errorf("Assert %s %s got: %v, want: %v:", c.input, cc.describe(), got, c.want)
+		}
+
+	}
+}
+
+func TestNotContainCase(t *testing.T) {
+	cases := []struct {
+		subString string
+		input     string
+		want      bool
+	}{
+		{subString: "t", input: "test", want: false},
+		{subString: "n", input: "test", want: true},
+	}
+
+	for _, c := range cases {
+		cc := newNotContainCase(c.subString)
+		got := cc.assert(c.input)
+		if got != c.want {
+			t.Errorf("Assert %s %s got: %v, want: %v:", c.input, cc.describe(), got, c.want)
+		}
+
+	}
+}
+
+func TestRegexCase(t *testing.T) {
+	cases := []struct {
+		subString string
+		input     string
+		want      bool
+	}{
+		{subString: "te.t", input: "test", want: true},
+		{subString: "te..t", input: "test", want: false},
+	}
+
+	for _, c := range cases {
+		cc := newRegexCase(c.subString)
+		got := cc.assert(c.input)
+		if got != c.want {
+			t.Errorf("Assert %s %s got: %v, want: %v:", c.input, cc.describe(), got, c.want)
+		}
+
+	}
+}
+
+func TestNotRegexCase(t *testing.T) {
+	cases := []struct {
+		subString string
+		input     string
+		want      bool
+	}{
+		{subString: "te.t", input: "test", want: false},
+		{subString: "te..t", input: "test", want: true},
+	}
+
+	for _, c := range cases {
+		cc := newNotRegexCase(c.subString)
+		got := cc.assert(c.input)
+		if got != c.want {
+			t.Errorf("Assert %s %s got: %v, want: %v:", c.input, cc.describe(), got, c.want)
+		}
+
 	}
 }
